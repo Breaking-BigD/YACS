649eca741950aa1be3a8ba08a62bf6d8937240e1
niksram <nikhilsram.off@gmail.com>
2020-11-28 12:28:38 +0530

    locks need to added, and logs need to implemented

--- master.py
+++ master.py
@@ -6,6 +6,13 @@ import sys
 import time
 
 
+def mergeDict(dict1, dict2):
+    d = dict()
+    d.update(dict1)
+    d.update(dict2)
+    return d
+
+
 class task_handler(threading.Thread):
 
     def __init__(self):
@@ -15,13 +22,13 @@ class task_handler(threading.Thread):
     def random_schedular(self):
         while True:
             random_worker = np.random.randint(0, 2)
-            if(Workers[workerIndex[random_worker]]['free_slots']):
+            if Workers[workerIndex[random_worker]]['free_slots']:
                 return workerIndex[random_worker]
 
     def round_robin(self):
         temp = 0
         while True:
-            if(Workers[workerIndex[self.robin_index]]['free_slots']):
+            if Workers[workerIndex[self.robin_index]]['free_slots']:
                 temp = self.robin_index
                 self.robin_index = (self.robin_index + 1) % len(workerIndex)
                 return workerIndex[temp]
@@ -34,23 +41,31 @@ class task_handler(threading.Thread):
             time.sleep(1)
 
     def run(self):
-        # acquire lock
-        new_task = TaskPool.pop(0)
-        # release lock
-        if(algo_type == 'RANDOM'):
-            free_worker = self.random_schedular()
-        elif(algo_type == 'RR'):
-            free_worker = self.round_robin()
-        elif(algo_type == 'LL'):
-            free_worker = self.least_loaded()
-        Workers[free_worker]['free_slots'] -= 1
-        workerName = ''
-        workerPort = Workers[free_worker]['port']
-        assignSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        assignSocket.connect((workerName, workerPort))
-        assignSocket.send(json.dumps(new_task).encode())
-        modifiedMessage = assignSocket.recv(1048576).decode()
-        assignSocket.close()
+        while(1):
+            if len(TaskPool):
+                # acquire lock
+                new_task = TaskPool.pop(0)
+                # release lock
+                if algo_type == 'RANDOM':
+                    free_worker = self.random_schedular()
+                elif algo_type == 'RR':
+                    free_worker = self.round_robin()
+                elif algo_type == 'LL':
+                    free_worker = self.least_loaded()
+                Workers[free_worker]['free_slots'] -= 1
+                Jobs[new_task["job_id"]][new_task["type"] +
+                                         "_tasks"][new_task["task_id"]]["status"] = 0
+                Jobs[new_task["job_id"]][new_task["type"] +
+                                         "_tasks"][new_task["task_id"]]["worker"] = free_worker
+                workerName = ''
+                workerPort = Workers[free_worker]['port']
+                assignSocket = socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM)
+                assignSocket.connect((workerName, workerPort))
+                print(new_task)
+                assignSocket.send(json.dumps(new_task).encode())
+                modifiedMessage = assignSocket.recv(1048576).decode()
+                assignSocket.close()
 
 
 class job_request_handler(threading.Thread):
@@ -58,21 +73,62 @@ class job_request_handler(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
+    def job_object_maker(self, s):
+        newJob = json.loads(s)
+        newJob["remaining_map_tasks"] = len(newJob["map_tasks"])
+        newJob["remaining_reduce_tasks"] = len(newJob["reduce_tasks"])
+        newJob["map_tasks"] = {i["task_id"]: mergeDict(
+            i, {"worker": None, "status": -1, "type": "map", "job_id": newJob["job_id"]}) for i in newJob["map_tasks"]}
+        newJob["reduce_tasks"] = {i["task_id"]: mergeDict(
+            i, {"worker": None, "status": -1, "type": "reduce", "job_id": newJob["job_id"]}) for i in newJob["reduce_tasks"]}
+        print("newjob", newJob)
+        return newJob
+
     def run(self):
         while 1:
             client, clientaddr = requestSocket.accept()
             message = client.recv(1048576)
-            modifiedMessage = message.decode().upper()
-            newJob = json.loads(modifiedMessage)
-            Jobs[newJob["JOB_ID"]] = {"map_tasks": newJob["MAP_TASKS"], "map_task_remaining": len(
-                newJob["MAP_TASKS"]), "reduce_tasks": newJob["REDUCE_TASKS"], "reduce_task_remaining": len(newJob["REDUCE_TASKS"])}
-            for i in Jobs[newJob["JOB_ID"]]["map_tasks"]:
-                elem = dict(i)
-                elem["job_id"] = newJob["JOB_ID"]
-                elem["type"] = "MAP"
+            newJob = self.job_object_maker(message.decode())
+            job_id = newJob["job_id"]
+            # acquire lock
+            Jobs[job_id] = newJob
+            # release lock
+            for elem in Jobs[job_id]["map_tasks"]:
                 # acquire lock
-                TaskPool.append(elem)
+                TaskPool.append(Jobs[job_id]["map_tasks"][elem])
                 # release lock
+            client.close()
+
+
+class worker_response_handler(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while 1:
+            worker, workeraddr = responseSocket.accept()
+            message = worker.recv(1048576)
+            completedTask = json.loads(message.decode())
+            for task in completedTask:
+                print("completed task", task)
+                # acquire lock
+                Workers[Jobs[task["job_id"]][task["type"] + "_tasks"]
+                        [task["task_id"]]["worker"]]["free_slots"] += 1
+                # release lock
+                Jobs[task["job_id"]][task["type"] +
+                                     "_tasks"][task["task_id"]]["status"] = 1
+                Jobs[task["job_id"]]["remaining_"+task["type"] + "_tasks"] -= 1
+                if(not Jobs[task["job_id"]]["remaining_map_tasks"] and not Jobs[task["job_id"]]["remaining_reduce_tasks"]):
+                    # acquire lock
+                    print('\n',"completed job",Jobs.pop(task["job_id"]),'\n')
+                    # release lock
+                elif(task["type"] == "map" and not Jobs[task["job_id"]]["remaining_map_tasks"]):
+                    for elem in Jobs[task["job_id"]]["reduce_tasks"]:
+                        # acquire lock
+                        TaskPool.append(Jobs[task["job_id"]]["reduce_tasks"][elem])
+                        # release lock
+            worker.close()
 
 
 algo_type = sys.argv[2]
@@ -81,7 +137,7 @@ f = open(sys.argv[1], 'r')
 workerConfig = json.load(f)
 Workers = {i['worker_id']: {'max_slots': i['slots'], 'port': i['port'],
                             'free_slots': i['slots']} for i in workerConfig["workers"]}
-workerIndex = sorted([i['worker_id'] for i in Workers])
+workerIndex = sorted([i for i in Workers])
 Jobs = dict()
 TaskPool = []
 
@@ -91,11 +147,19 @@ requestSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 requestSocket.bind(('', requestPort))
 requestSocket.listen(1)
 
+responsePort = 5001
+responseSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+responseSocket.bind(('', responsePort))
+responseSocket.listen(1)
+
 job_handle = job_request_handler()
 task_spawn = task_handler()
+response_handle=worker_response_handler()
 job_handle.start()
 task_spawn.start()
+response_handle.start()
 job_handle.join()
 task_spawn.join()
+response_handle.join()
 
 requestSocket.close()
--- worker.py
+++ worker.py
@@ -1,9 +1,10 @@
 import json
-import numpy as np 
+import numpy as np
 import socket
 import threading
 import sys
 import time
+
 
 class task_executer(threading.Thread):
 
@@ -12,46 +13,57 @@ class task_executer(threading.Thread):
 
     def run(self):
         while 1:
-            if len(execution_slots):
-                message_list=[]
-                for curr_slot in execution_slots:
-                    # acquire lock
+            message_list = []
+            for curr_slot in range(len(execution_slots)):
+                # acquire lock
+                if(execution_slots[curr_slot]):
                     execution_slots[curr_slot]["duration"] -= 1
                     if not execution_slots[curr_slot]["duration"]:
-                        message_list.append(curr_slot)
-                    # release lock
-                time.sleep(1)
-                if(len(message_list)):
-                    pop_list = pop_list[::-1]
-                    message = json.dumps(message_list)
-                    masterName = ''
-                    masterPort = 5001
-                    updateSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    updateSocket.connect((masterName, masterPort))
-                    updateSocket.send(message.encode())
-                    modifiedMessage = updateSocket.recv(1048576).decode()
-                    updateSocket.close()
+                        message_list.append(execution_slots[curr_slot])
+                        execution_slots[curr_slot]=None
+                # release lock
+            time.sleep(1)
+            if(len(message_list)):
+                print(message_list)
+                message = json.dumps(message_list)
+                masterName = ''
+                masterPort = 5001
+                updateSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                updateSocket.connect((masterName, masterPort))
+                updateSocket.send(message.encode())
+                updateSocket.close()
 
-class job_handler(threading.Thread):
+
+class slot_handler(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
         while 1:
-            master,masteraddr=requestSocket.accept()
+            master, masteraddr = requestSocket.accept()
             message = master.recv(1048576)
-            modifiedMessage = message.decode().upper()
-            newTask = json.loads(modifiedMessage)
+            modifiedMessage = message.decode()
+            newtask = json.loads(modifiedMessage)
             # acquire lock
-            for curr_slot in execution_slots:
-                execution_slots[curr_slot]=newTask 
-            # release lock           
+            c = 0
+            while(execution_slots[c] != None):
+                c += 1
+            execution_slots[c] = newtask
+            # release lock
             master.close()
-            
+
+
 total_slots = int(sys.argv[2])
-execution_slots={i:None for i in range(total_slots)}
+execution_slots = [ None for i in range(total_slots)]
 requestPort = int(sys.argv[1])
 requestSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 requestSocket.bind(('', requestPort))
 requestSocket.listen(1)
+
+slot_handle = slot_handler()
+task_spawn = task_executer()
+slot_handle.start()
+task_spawn.start()
+slot_handle.join()
+task_spawn.join()
